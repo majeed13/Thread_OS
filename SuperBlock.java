07b4287c1cd9de373d5c313542ddf63a9cf7a7c1
majeed13 <43226186+majeed13@users.noreply.github.com>
2019-06-05 10:37:10 -0700

    Add files via upload
    Added the skeleton code for the classes we need to implement

---
+++ SuperBlock.java
@@ -0,0 +1,11 @@
+
+class Superblock {
+   public int totalBlocks; // the number of disk blocks
+   public int totalInodes; // the number of inodes
+   public int freeList;    // the block number of the free list's head
+   
+   public SuperBlock( int diskSize ) {
+	...
+   }
+   ...	
+}
